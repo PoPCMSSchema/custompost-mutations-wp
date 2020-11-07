1ae530d53fbc0f22becf6daa08f30d0eb42af56c
Leonardo Losoviz <leo@getpop.org>
2020-11-07 13:13:12 +0800

    Moved functions to typeAPI

--- src/TypeAPIs/CustomPostTypeAPI.php
+++ src/TypeAPIs/CustomPostTypeAPI.php
@@ -5,18 +5,59 @@ declare(strict_types=1);
 namespace PoPSchema\CustomPostMutationsWP\TypeAPIs;
 
 use PoPSchema\CustomPostMutations\TypeAPIs\CustomPostTypeAPIInterface;
+use PoPSchema\CustomPostsWP\TypeAPIs\CustomPostTypeAPIUtils;
 
 /**
  * Methods to interact with the Type, to be implemented by the underlying CMS
  */
 class CustomPostTypeAPI implements CustomPostTypeAPIInterface
 {
+    protected function convertQueryArgsFromPoPToCMSForInsertUpdatePost(array &$query): void
+    {
+        // Convert the parameters
+        if (isset($query['custom-post-status'])) {
+            $query['post_status'] = CustomPostTypeAPIUtils::convertPostStatusFromPoPToCMS($query['custom-post-status']);
+            unset($query['custom-post-status']);
+        }
+        if (isset($query['id'])) {
+            $query['ID'] = $query['id'];
+            unset($query['id']);
+        }
+        if (isset($query['post-content'])) {
+            $query['post_content'] = $query['post-content'];
+            unset($query['post-content']);
+        }
+        if (isset($query['post-title'])) {
+            $query['post_title'] = $query['post-title'];
+            unset($query['post-title']);
+        }
+        if (isset($query['post-categories'])) {
+            $query['post_category'] = $query['post-categories'];
+            unset($query['post-categories']);
+        }
+        if (isset($query['custom-post-type'])) {
+            $query['post_type'] = $query['custom-post-type'];
+            unset($query['custom-post-type']);
+        }
+    }
     /**
      * @param array<string, mixed> $data
      * @return mixed the ID of the created custom post
      */
     public function createCustomPost(array $data)
     {
-        return null;
+        // Convert the parameters
+        $this->convertQueryArgsFromPoPToCMSForInsertUpdatePost($data);
+        return \wp_insert_post($data);
+    }
+    /**
+     * @param array<string, mixed> $data
+     * @return mixed the ID of the updated custom post
+     */
+    public function updateCustomPost(array $data)
+    {
+        // Convert the parameters
+        $this->convertQueryArgsFromPoPToCMSForInsertUpdatePost($data);
+        return \wp_update_post($data);
     }
 }
